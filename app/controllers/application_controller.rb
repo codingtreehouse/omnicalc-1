a978c7750f1dd12750ab3341c0cf1e7706d53ef2
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-06 16:26:46 +0000

    updated the square root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,20 @@ class ApplicationController < ActionController::Base
     render({:template => "calculation_templates/rand_new.html.erb"})
   end
 
-
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
     @rand = rand(@lower..@upper)
     render({:template => "calculation_templates/rand_results.html.erb"})
+  end
+
+  def sqrt_form
+    render({:template => "calculation_templates/sqrt_form.html.erb"})
+  end
+
+  def sqrt_results
+    @number = params.fetch("sqrt").to_f
+    @sqrt = sqrt(@number)
+    render({:template => "calculation_templates/sqrt_results.html.erb"})
   end
 end
